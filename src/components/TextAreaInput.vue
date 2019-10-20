56586104a1899f5059ed4d57d82e3a68ce333a8d
Maks Surguy <msurguy@gmail.com>
2019-10-19 23:16:02 -0700

    added multiline

---
+++ src/components/TextAreaInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="sidebar-control">
+    <div class="control-header">
+      <div>
+        <svgicon v-if="leftIcon" :name="leftIcon.icon" :width="leftIcon.width" :height="leftIcon.height" color="#FFFFFF" :fill="false"></svgicon>
+      </div>
+      <div class="control-label">
+        {{ label }}
+      </div>
+      <div>
+        <svgicon v-if="rightIcon" :name="rightIcon.icon" :width="rightIcon.width" :height="rightIcon.height" color="#FFFFFF" :fill="false"></svgicon>
+      </div>
+    </div>
+    <div class="control-header">
+      <textarea class="control-textarea-input" rows="4" :value="value" @change="onInput"></textarea>
+      <a v-if="showReset" class="btn" @click="$emit('reset')">reset</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import '@/icons'
+
+export default {
+  name: 'TextAreaInput',
+  props: {
+    leftIcon: {
+      type: Object
+    },
+    rightIcon: {
+      type: Object
+    },
+    label: {
+      type: String,
+      default: 'Label'
+    },
+    value: {
+      type: String,
+      default: ''
+    },
+    showReset: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    onInput ($event) {
+      this.$emit('input', $event.target.value)
+    }
+  }
+}
+</script>
